2317fedd6d79bf46697dc01c7542833dcffa5462
naman360 <naman.geek@gmail.com>
2024-06-09 20:30:24 +0530

    feat: added progress bar for timer

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/toast.module.css";
 
 export type ToastType = "success" | "error" | "info" | "warning";
@@ -20,17 +21,41 @@ type Props = ToastDataType;
 const Toast = ({
   type = "info",
   message = "This is an info",
+  duration,
   onClose,
 }: Props) => {
+  const [timerWidth, setTimerWidth] = useState(100);
+  const timerRef = useRef<number>(duration);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timerRef.current > 0) {
+        timerRef.current -= 100;
+        setTimerWidth((timerRef.current / duration) * 100);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [duration]);
+
   const handleClose = () => {
     if (typeof onClose === "function") onClose();
   };
+
   return (
     <div className={`${styles.toastContainer} ${styles[type]}`}>
-      {message}
-      <span onClick={() => handleClose()} className={styles.closeButton}>
-        &times;
-      </span>
+      <div className={`${styles.toastInfo}`}>
+        <span>{message}</span>
+        <span onClick={() => handleClose()} className={styles.closeButton}>
+          &times;
+        </span>
+      </div>
+      <div
+        className={`${styles.timer}`}
+        style={{
+          width: `${timerWidth}%`,
+        }}
+      ></div>
     </div>
   );
 };
